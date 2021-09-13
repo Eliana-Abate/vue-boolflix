b283760124bca43933175356098bffb71ad8d687
Eliana-Abate <eliana.abate@outlook.it>
2021-09-14 01:02:49 +0200

    added v-if/v-else for display a message when arrayMovies or arraySeries are empty

--- src/components/Films.vue
+++ src/components/Films.vue
@@ -2,14 +2,21 @@
   <section class="container">
     <div>
       <h3 class="py-3">Movies</h3>
-
-      <FilmCard :arrayFromFather="arrayMovies" :typeFilm="listFilm" />
+      <div v-if="arrayMovies == ''">
+        <p class="y-msg">Non sono presenti Film nella lista...</p>
+        <p>Effettua una nuova ricerca per questa categoria.</p>
+      </div>
+      <FilmCard v-else :arrayFromFather="arrayMovies" :typeFilm="listFilm" />
     </div>
 
     <div class="pt-5">
       <h3 class="py-3">Serie Tv</h3>
+      <div v-if="arraySeries == ''">
+        <p class="y-msg">Non sono presenti Serie TV nella lista...</p>
+        <p>Effettua una nuova ricerca per questa categoria.</p>
+      </div>
 
-      <FilmCard :arrayFromFather="arraySeries" :typeFilm="listSeries" />
+      <FilmCard v-else :arrayFromFather="arraySeries" :typeFilm="listSeries" />
     </div>
   </section>
 </template>
@@ -36,4 +43,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.y-msg {
+  color: rgb(255, 208, 0);
+}
 </style>
